b6e39a79e2ee559d59620715505d3eea6d154be5
rafajahan7 <rafa.jahan7@gmail.com>
2026-07-18 22:34:36 +0600

    js practice loops

---
+++ intro-to-JS/first.js
@@ -0,0 +1,60 @@
+// let hello="Hello JavaScript!";
+// console.log(hello);
+
+// let intro="My name is Alex.";
+// console.log(intro);
+
+// let web="I am excited to become a Web Developer."
+// console.log(web);
+
+// first task
+
+
+// let num1=25;
+// let num2=10;
+
+// console.log("Addition:", num1+num2);
+// console.log("Subtraction:", num1-num2);
+// console.log("Multiplication:", num1*num2);
+// console.log("Division:", num1/num2);
+// console.log("Modulus:", num1%num2);
+
+// second task
+
+// let studentName="Alex Johnson";
+// let age=20;
+// let country="Bangladesh";
+// let isStudent=true;
+// console.log("Student Name:", studentName);
+// console.log("Age:", age);
+// console.log("Country:", country);
+// console.log("He is student:", isStudent);
+
+// third task
+
+// let a=100;
+// let b="javascript";
+// let c=false;
+// let d="3.14";
+// console.log("a:", typeof a);
+// console.log("b:", typeof b);
+// console.log("c:", typeof c);
+// console.log("d:", typeof d);
+
+// loop task 1(print 1 to 15)
+
+// for( let i=1;i<=15;i++){
+//     console.log(i);
+// }
+
+// loop task 2(print every 3rd number from 0 to 30)
+
+// for(let i=0;i<=30; i+=3){
+//     console.log("from 0 to 30", i);
+// }
+
+// loop task 3(evens between 20 to 30)
+
+// for(let i=20; i<=30; i+=2){
+//     console.log("even numbers are", i);
+// }
---
+++ intro-to-JS/loop.js
@@ -0,0 +1,29 @@
+// 3 loop tasks are in first.js
+
+// loop task 4(countdown from 20)
+
+// for(let i=20; i>=0; i--){
+//     console.log("Go", i);
+// }
+
+// loop task 5(reverse odd numbers from 9 to 1)
+
+// let i=9;
+// while(i>=0){
+    
+//     console.log(i);
+//     i-=2;
+// }
+
+// for(let i=9; i>=0; i-=2){
+//     console.log(i);
+// }
+
+// loop task 6(start at 100 and subtract by 5 each time till 0)
+
+let i=100;
+while(i>=0){
+    console.log(i);
+    i-=5;
+}
+
